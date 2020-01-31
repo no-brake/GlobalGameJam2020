f622750617ceffe02e260221953024711c6f0ba1
Kim Mannstedt <k.mannstedt@gmail.com>
2020-01-31 21:31:42 +0100

    use fake path to hide real structure

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 var express = require('express');
 var app = express();
 
+app.use('/public', express.static('dist'));
+app.use('/public', express.static('node_modules/react/umd/'));
+app.use('/public', express.static('node_modules/react-dom/umd/'));
+
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.sendFile(__dirname + '/index.html');
 });
 
 app.listen(8080, function () {
-  console.log('Example app listening on port 3000!');
+  console.log('Example app listening on port 8080!');
 });
-

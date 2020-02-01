0a71fb7c8099bd70cefad199de97a598d240387d
Kim Mannstedt <k.mannstedt@gmail.com>
2020-02-01 02:07:17 +0100

    init item component

---
+++ src/components/Item.tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+
+export interface ItemProps {}
+export interface ItemState {}
+
+// 'HelloProps' describes the shape of props.
+// State is never set so we use the '{}' type.
+export class ItemButton extends React.Component<ItemProps, ItemState> {
+    render() {
+        return <button className="base-button item-button">Add</button>;
+    }
+}

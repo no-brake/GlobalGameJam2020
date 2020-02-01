deaff1b9b88948f3c2700d651a2c7435a323ccd9
Kim Mannstedt <k.mannstedt@gmail.com>
2020-02-01 02:10:32 +0100

    added ItemManager

--- src/components/Item.tsx
+++ src/components/Item.tsx
@@ -3,8 +3,6 @@ import * as React from "react";
 export interface ItemProps {}
 export interface ItemState {}
 
-// 'HelloProps' describes the shape of props.
-// State is never set so we use the '{}' type.
 export class Item extends React.Component<ItemProps, ItemState> {
     render() {
         return <button className="base-button item-button">Add</button>;
---
+++ src/components/ItemManager.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+
+export interface ItemManagerProps {}
+export interface ItemManagerState {}
+
+export class ItemManager extends React.Component<ItemManagerProps, ItemManagerState> {
+    render() {
+        return <div className="item-manager"></div>
+    }
+}

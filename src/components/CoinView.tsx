bca434cdcc25932c6d90b8cef67644dfd2823698
Kristof <kabumm27@live.de>
2020-02-01 20:58:52 +0100

    It's raining money!

--- src/components/CoinView.tsx
+++ src/components/CoinView.tsx
@@ -6,6 +6,19 @@ export interface CoinViewProps { game: Game }
 export class CoinView extends React.Component<CoinViewProps, {}> {
     render() {
         const coins = this.props.game.coins;
-        return <div className="coins white-text">Coins: {coins}</div>;
+        const coinUpdates = this.props.game.coinUpdates;
+
+        const floatingCoins = [];
+        for (const update of coinUpdates) {
+            const top = (30 + update.offsetY) + "px";
+            const left = (1100 + update.offsetX) + "px";
+            const fontSize = Math.max(10, Math.min(30, 20 + (update.amount / 10))) + "px";
+            floatingCoins.push(<div className="floating-coin" style={{top, left, fontSize}}>{update.amount}</div>);
+        }
+
+        return <div>
+                <div className="coins white-text">Coins: {coins}</div>
+                {floatingCoins}
+            </div>;
     }
 }

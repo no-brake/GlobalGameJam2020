a10323ace54a703e03f779f5b7fd41888fb8e662
Kim Mannstedt <k.mannstedt@gmail.com>
2020-02-01 02:07:42 +0100

    minor changes

--- src/components/Item.tsx
+++ src/components/Item.tsx
@@ -5,7 +5,7 @@ export interface ItemState {}
 
 // 'HelloProps' describes the shape of props.
 // State is never set so we use the '{}' type.
-export class ItemButton extends React.Component<ItemProps, ItemState> {
+export class Item extends React.Component<ItemProps, ItemState> {
     render() {
         return <button className="base-button item-button">Add</button>;
     }

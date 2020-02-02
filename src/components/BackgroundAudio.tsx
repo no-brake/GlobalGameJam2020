cd271784b06593947b1bcd8e78c774a314b036e8
Julian Gill <julian.gill@web.de>
2020-02-02 15:12:43 +0100

    Merge

--- src/components/BackgroundAudio.tsx
+++ src/components/BackgroundAudio.tsx
@@ -7,10 +7,14 @@ export interface BackgroundAudioState { }
 export class BackgroundAudio extends React.Component<BackgroundAudioProps, BackgroundAudioState>{
     private myRef = React.createRef<HTMLAudioElement>();
 
-    MuteAudio(e:React.MouseEvent<HTMLElement>) {
+    MuteAudio(e: React.MouseEvent<HTMLElement>) {
         this.myRef.current.muted = !this.myRef.current.muted;
-        const child:HTMLElement = e.currentTarget.firstChild as HTMLElement;
+        const child: HTMLElement = e.currentTarget.firstChild as HTMLElement;
         child.innerText = child.innerText == 'volume_off' ? 'volume_up' : 'volume_off'
+    }
+
+    ShowCredits() {
+
     }
 
     render() {
@@ -19,6 +23,10 @@ export class BackgroundAudio extends React.Component<BackgroundAudioProps, Backg
                 <button onClick={(e) => this.MuteAudio(e)}>
                     <i className="material-icons">volume_off</i>
                 </button>
+                <button onClick={() => this.ShowCredits()} className="base-button credits-icon">
+                    <img className="credits-icon" src="/public/assets/icons/credits-white-outline.png" />
+                </button>
+
                 <audio id="background audio" autoPlay loop ref={this.myRef}>
                     <source src="./public/assets/sounds/bgsound/chillstep_2.wav" type="audio/wav" />
                 </audio>

a8d852497095c5491c1898666f5e710e0867fe7d
Julian Gill <julian.gill@web.de>
2020-02-02 11:17:00 +0100

    CoinView

--- src/components/CoinView.tsx
+++ src/components/CoinView.tsx
@@ -13,12 +13,15 @@ export class CoinView extends React.Component<CoinViewProps, {}> {
             const top = (30 + update.offsetY) + "px";
             const left = (1100 + update.offsetX) + "px";
             const fontSize = Math.max(10, Math.min(30, 20 + (update.amount / 10))) + "px";
-            floatingCoins.push(<div className="floating-coin" key={"coin" + top + left} style={{top, left, fontSize}}>{update.amount}</div>);
+            floatingCoins.push(<div className="floating-coin" key={"coin" + top + left} style={{ top, left, fontSize }}>{update.amount}</div>);
         }
 
         return <div>
-                <div className="coins white-text">Coins: {coins}</div>
-                {floatingCoins}
-            </div>;
+            <div className="coins-container">
+                <img src="./public/assets/items/150x150-coins-multiple.png" className="coins-image" />
+                <div className="coins white-text">{coins}</div>
+            </div>
+            {floatingCoins}
+        </div>;
     }
 }

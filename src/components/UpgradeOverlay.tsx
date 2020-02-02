52da7fe22ecd99efa49cfc7bc81d5b8bd5e51294
Kristof <kabumm27@live.de>
2020-02-02 12:41:27 +0100

    Added upgrade overlay

---
+++ src/components/UpgradeOverlay.tsx
@@ -0,0 +1,19 @@
+import * as React from "react";
+import { Game } from "game/Game";
+
+export interface UpgradeOverlayProps { game: Game, closeHandler: () => void }
+export interface UpgradeOverlayState { }
+
+export class UpgradeOverlay extends React.Component<UpgradeOverlayProps, UpgradeOverlayState> {
+
+    render() {
+        const game = this.props.game;
+
+        const upgrades = ["Bigger", "Faster", "Better", "+1", "+2", "+3", "+99", "Automate Everything", "Do stuff", "I Win"]
+
+        return <div className="upgrade-overlay">
+            <button className="close-button" onClick={() => this.props.closeHandler()}>Close</button>
+            {upgrades.map(u => <div key={u}><img src="public/assets/icons/100x100-upgrade-icon.png" /><div className="text">{u}</div></div>)}
+        </div>;
+    }
+}
